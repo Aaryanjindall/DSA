475b1c51bc5d9eaabd80aa0bd0acdc1fd905c7b7
AARYAN JINDAL <aaryanjindal15@gmail.com>
2025-02-19 13:09:28 +0530

    19feb

---
+++ LINKED LIST/LinkedList4.java
@@ -0,0 +1,21 @@
+public class LinkedList4 {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void addmid(int idx , int data){
+        Node newnode = 
+
+    }
+    public static void main(String[] args) {
+        LinkedList4 ll = new LinkedList4();
+        ll.addmid()
+    }
+}
