38dd7c18e9599418866ab796d997ff48bb442f52
AARYAN JINDAL <aaryanjindal15@gmail.com>
2025-06-10 23:37:00 +0530

    10 june 2025

---
+++ GRAPHS/bfs.java
@@ -0,0 +1,17 @@
+public class bfs {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(
+            int s , int d , int w
+        ){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+
+    }
+    
+}
